70b718296c344917a20a70998370c06fe9df78a2
Brandon Caples <brandoncaples@gmail.com>
2018-03-15 18:29:13 -0500

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :lists do
-  	resources :basics, except: [:index]
-    resources :products, except: [:index]
-  end
+	resources :lists do
+		resources :basics, except: [:index]
+		resources :products, except: [:index]
+	end
 
-  devise_for :users
-  
-  root 'welcome#index'
+	devise_for :users
+
+	get 'about' => 'welcome#about'
+	root 'welcome#index'
 end
